c9dc2c9df39b9c3151a0e273afc54819cd639e9c
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2022-12-13 12:59:46 +0100

    Changed default collections directory

--- data_adapter/settings.py
+++ data_adapter/settings.py
@@ -8,7 +8,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
 
 ROOT_DIR = pathlib.Path(__file__).parent.parent
-COLLECTIONS_DIR = ROOT_DIR / "collections"
+COLLECTIONS_DIR = pathlib.Path.cwd() / "collections"
 
 DATABUS_ENDPOINT = "https://energy.databus.dbpedia.org/sparql"
 
