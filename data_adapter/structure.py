ac277dc2b1b7c54c96fd530e5f2004979b3707c6
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2022-12-13 14:39:26 +0100

    Processes can be get from structure

--- data_adapter/structure.py
+++ data_adapter/structure.py
@@ -1,19 +1,26 @@
+HARDCODED_ES_STRUCTURE = {
+    "energy transformation unit": {
+        "input_ratio": {"inputs": ["gas"], "outputs": ["electricity"]},
+        "output_ratio": {"inputs": ["gas"], "outputs": ["electricity"]},
+        "emission_factor": {"inputs": ["gas"], "outputs": ["co2"]},
+    },
+    "battery storage": {
+        "input_ratio": {"inputs": ["electricity"], "outputs": ["electricity"]},
+        "output_ratio": {"inputs": ["electricity"], "outputs": ["electricity"]},
+        "e2p_ratio": {"inputs": ["electricity"], "outputs": []},
+    },
+    "onshore wind farm": {
+        "installed_capacity": {
+            "inputs": ["onshore wind farm"],  # wind timeseries
+            "outputs": ["electricity"],
+        }
+    },
+}
+
+
 def get_energy_structure():
-    return {
-        "energy transformation unit": {
-            "input_ratio": {"inputs": ["gas"], "outputs": ["electricity"]},
-            "output_ratio": {"inputs": ["gas"], "outputs": ["electricity"]},
-            "emission_factor": {"inputs": ["gas"], "outputs": ["co2"]},
-        },
-        "battery storage": {
-            "input_ratio": {"inputs": ["electricity"], "outputs": ["electricity"]},
-            "output_ratio": {"inputs": ["electricity"], "outputs": ["electricity"]},
-            "e2p_ratio": {"inputs": ["electricity"], "outputs": []},
-        },
-        "onshore wind farm": {
-            "installed_capacity": {
-                "inputs": ["onshore wind farm"],
-                "outputs": ["electricity"],
-            }
-        },
-    }
+    return HARDCODED_ES_STRUCTURE
+
+
+def get_processes():
+    return list(HARDCODED_ES_STRUCTURE)
