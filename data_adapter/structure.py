c4a185f2336cab5479914af56137d03a768a91e5
FelixMau <fmau@posteo.de>
2023-01-06 16:15:49 +0100

    With loops

--- data_adapter/structure.py
+++ data_adapter/structure.py
@@ -1,3 +1,6 @@
+import networkx as nx
+import jmespath
+import matplotlib.pyplot as plt
 from collections import defaultdict, namedtuple
 
 
@@ -42,3 +45,59 @@ def get_energy_structure():
 
 def get_processes():
     return list(HARDCODED_ES_STRUCTURE)
+
+
+def draw_struct(HARDCODED_ES_STRUCTURE: dict, ADDITONAL_PARAMETERS: dict)-> nx.Graph:
+    """
+
+    Parameters
+    ----------
+    HARDCODED_ES_STRUCTURE
+    ADDITONAL_PARAMETERS
+
+    Returns
+    -------
+    plottable Networkx Graph illustrating the Models structure given to the Frameworks
+
+    """
+
+    def ckeck_list_singular(lst)->bool:
+        ele = lst[0]
+        chk = True
+        # Comparing each element with first item
+        for item in lst:
+            if ele != item:
+                chk = False
+                break
+        return chk
+
+    # Empty Graph:
+    G = nx.DiGraph()
+    edges = []
+    for unit, process in HARDCODED_ES_STRUCTURE.items():
+        if ckeck_list_singular(jmespath.search("@.*.inputs[]", HARDCODED_ES_STRUCTURE[unit])) and\
+                ckeck_list_singular(jmespath.search("@.*.outputs[]", HARDCODED_ES_STRUCTURE[unit])):
+            for process_name, process_parameters in process.items():
+                edges.append((unit, process_name))
+                edges.append((unit, process_parameters["inputs"][0]))
+                edges.append((unit, process_parameters["outputs"][0]))
+                break
+        elif ckeck_list_singular(jmespath.search("@.*.inputs[]", HARDCODED_ES_STRUCTURE[unit])) and not\
+                ckeck_list_singular(jmespath.search("@.*.outputs[]", HARDCODED_ES_STRUCTURE[unit])):
+            for process_name, process_parameters in process.items():
+                edges.append((unit, process_name))
+                edges.append((unit, process_parameters["inputs"][0]))
+                edges.append((process_name, process_parameters["outputs"][0]))
+        else:
+            for process_name, process_parameters in process.items():
+                edges.append((unit, process_name))
+                edges.append((process_name, process_parameters["inputs"][0]))
+                edges.append((process_name, process_parameters["outputs"][0]))
+    print(edges)
+    G.add_edges_from(edges)
+
+    plt.figure(figsize=(6, 4))
+    nx.draw_networkx(G)
+    plt.show()
+
+draw_struct(HARDCODED_ES_STRUCTURE=HARDCODED_ES_STRUCTURE, ADDITONAL_PARAMETERS=ADDITONAL_PARAMETERS)
