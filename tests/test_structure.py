d4fddebbcaa5f6edb3d0db96e0d281f38f6b53e3
chrwm <christoph.muschner@rl-institut.de>
2023-02-16 15:12:07 +0100

    Update test #2

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -39,7 +39,8 @@ def test_get_energy_structure():
             "eff_heat": {"inputs": ["gas", "coal"], "outputs": ["heat", "co2"]},
             "eff_elec": {"inputs": ["biomass", "gas"], "outputs": ["elec", "co2"]},
         },
-        "test": {"para3": {"inputs": ["in"], "outputs": ["out"]}},
+        "empty_out": {"para": {"inputs": ["in1", "in2"], "outputs": []}},
+        "empty_in": {"para": {"inputs": [], "outputs": ["out"]}},
     }
 
     assert (
