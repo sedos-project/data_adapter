1334ee2becf8cddd930995ed8b03452be879206c
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-02-20 16:07:06 +0100

    Refactored get_energy_structure to lookup structure in structures folder

--- data_adapter/structure.py
+++ data_adapter/structure.py
@@ -1,74 +1,20 @@
 from __future__ import annotations
 
-import json
-from collections import defaultdict, namedtuple
-
 import re
 from collections import defaultdict, namedtuple
-from pathlib import Path
 
 import pandas as pd
-import sqlalchemy as sa
-from sqlalchemy.orm import declarative_base, relationship
 
 from data_adapter import settings
 
-# constants
 MAX_IDENTIFIER_LENGTH = 50
+
 # pylint:disable=C0209
-IDENTIFIER_PATTERN = re.compile(
-    "^[a-z][a-z0-9_, ]{0,%s}$" % (MAX_IDENTIFIER_LENGTH - 1)
-)
+IDENTIFIER_PATTERN = re.compile("^[a-z][a-z0-9_, ]{0,%s}$" % (MAX_IDENTIFIER_LENGTH - 1))
 
 
 class StructureError(Exception):
     """Raised if structure is corrupted"""
-
-
-with open(settings.STRUCTURES_DIR / "HARDCODED_ES_STRUCT.json", "r") as hardcoded_es_file:
-    HARDCODED_ES_STRUCTURE = json.load(hardcoded_es_file)
-
-
-Base = declarative_base()
-
-parameter_inputs = sa.Table(
-    "parameter_inputs",
-    Base.metadata,
-    sa.Column("parameter_id", sa.ForeignKey("parameter.id"), primary_key=True),
-    sa.Column("input_id", sa.ForeignKey("input_output.id"), primary_key=True),
-)
-
-parameter_outputs = sa.Table(
-    "parameter_outputs",
-    Base.metadata,
-    sa.Column("parameter_id", sa.ForeignKey("parameter.id"), primary_key=True),
-    sa.Column("output_id", sa.ForeignKey("input_output.id"), primary_key=True),
-)
-
-
-class Process(Base):
-    __tablename__ = "process"
-
-    id = sa.Column(sa.Integer, primary_key=True)  # noqa: A003
-    name = sa.Column(sa.String)
-    parameters: Parameter = relationship("Parameter")
-
-
-class Parameter(Base):
-    __tablename__ = "parameter"
-
-    id = sa.Column(sa.Integer, primary_key=True)  # noqa: A003
-    process_id = sa.Column(sa.BIGINT, sa.ForeignKey("process.id"), nullable=False)
-    name = sa.Column(sa.String)
-    inputs: InputOutput = relationship("InputOutput", secondary=parameter_inputs, backref="to_parameters")
-    outputs: InputOutput = relationship("InputOutput", secondary=parameter_outputs, backref="from_parameters")
-
-
-class InputOutput(Base):
-    __tablename__ = "input_output"
-
-    id = sa.Column(sa.Integer, primary_key=True)  # noqa: A003
-    name = sa.Column(sa.String)
 
 
 AdditionalParameter = namedtuple("AdditionalParameter", ("subject", "isAbout"))
@@ -109,28 +55,26 @@ def check_character_convention(dataframe: pd.DataFrame):
             if not isinstance(element, str):
                 continue
             if not IDENTIFIER_PATTERN.match(element):
-                raise ValueError(
-                    f"Wrong syntax: {element}\nAllowed are characters: a-z and 0-9 and , and _"
-                )
+                raise ValueError(f"Wrong syntax: {element}\nAllowed are characters: a-z and 0-9 and , and _")
 
 
-def get_energy_structure(process_parameter_path: str) -> dict:
+def get_energy_structure(structure: str) -> dict:
     """
     Parse processes and its parameters with corresponding inputs and outputs to dict.
 
     Parameters
     ----------
-    process_parameter_path: str
-        Path to process and parameter sheet.
+    structure: str
+        Name of structure to look up in structure folder
 
     Returns
     -------
     dict
         Energy modelling processes, its parameters and inputs and output
     """
-
+    structure_file = settings.STRUCTURES_DIR / f"{structure}.csv"
     process_parameter_in_out = pd.read_csv(
-        filepath_or_buffer=process_parameter_path,
+        filepath_or_buffer=structure_file,
         delimiter=";",
         encoding="utf-8",
         usecols=["parameter", "process", "inputs", "outputs"],
@@ -159,16 +103,10 @@ def get_energy_structure(process_parameter_path: str) -> dict:
         if dic.get("process") not in es_structure:
             es_structure[dic.get("process")] = dic_para
         else:
-            es_structure[dic.get("process")] = (
-                es_structure[dic.get("process")] | dic_para
-            )
+            es_structure[dic.get("process")] = es_structure[dic.get("process")] | dic_para
 
     return es_structure
 
 
-def get_processes():
-    return list(HARDCODED_ES_STRUCTURE)
-
-
-def init_database():
-    Base.metadata.create_all(settings.engine)
+def get_processes(structure: str):
+    return list(get_energy_structure(structure))
--- tests/test_structure.py
+++ tests/test_structure.py
@@ -1,39 +1,7 @@
-import os
-
-import pytest
-from sqlalchemy.orm import Session
-
-from data_adapter import settings, structure
 from data_adapter.structure import get_energy_structure
 
 
-@pytest.fixture()
-def _setup_db():
-    structure.init_database()
-    yield
-    # Remove test.db after tests
-    if settings.DATABASE_URL.startswith("sqlite"):
-        test_db = settings.DATABASE_URL.split("///")[1]
-        os.remove(test_db)  # Set breakpoint here to check sqlite DB before removal
-
-
-@pytest.mark.usefixtures("_setup_db")
-def test_es_example():
-    with Session(settings.engine) as session:
-        gas_turbine = structure.Process(name="gas turbine")
-        gas_bus = structure.InputOutput(name="gas")
-        electricity_bus = structure.InputOutput(name="electricity")
-        session.add_all((gas_bus, electricity_bus, gas_turbine))
-        session.flush()
-        input_ratio = structure.Parameter(name="input_ratio", process_id=gas_turbine.id)
-        input_ratio.inputs.append(gas_bus)
-        input_ratio.outputs.append(electricity_bus)
-        session.add(input_ratio)
-        session.commit()
-
-
 def test_get_energy_structure():
-
     dict_expected = {
         "chp": {
             "eff_heat": {"inputs": ["gas", "coal"], "outputs": ["heat", "co2"]},
@@ -43,9 +11,4 @@ def test_get_energy_structure():
         "empty_in": {"para": {"inputs": [], "outputs": ["out"]}},
     }
 
-    assert (
-        get_energy_structure(
-            process_parameter_path="test_data/test_mimo/user_mimos.csv"
-        )
-        == dict_expected
-    )
+    assert get_energy_structure(structure="user_mimos") == dict_expected
