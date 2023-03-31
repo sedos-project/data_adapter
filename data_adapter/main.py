9ab15dda97805bbc979e67c1129ebfab1af69233
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-03-31 10:08:15 +0200

    Add links to get_process function in main.py

--- data_adapter/main.py
+++ data_adapter/main.py
@@ -7,12 +7,12 @@ def download_collection(collection_url):
     databus.download_collection(collection_url)
 
 
-def get_process(collection, process):
-    df = preprocessing.get_process(collection, process)
+def get_process(collection, process, links):
+    df = preprocessing.get_process(collection, process, links)
     print(df)
 
 
 if __name__ == "__main__":
     command = sys.argv[1]
     if command == "get_process":
-        get_process(sys.argv[2], sys.argv[3])
+        get_process(sys.argv[2], sys.argv[3], sys.argv[4])
