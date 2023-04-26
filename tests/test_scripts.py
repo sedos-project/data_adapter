77c22a80ce2c848479bbd6720da399039dec364e
chrwm <christoph.muschner@rl-institut.de>
2023-04-26 18:15:41 +0200

    Update test

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,7 +1,17 @@
-from data_adapter.scripts.sedos_structure_parser import parse_es_structure
+import pandas as pd
+
+from scripts.sedos_structure_parser import parse_es_structure
 
 
 def test_parse_es_structure():
+    expected_output = pd.read_excel(r"test_data\test_structures\SEDOS_es_structure.xlsx")
 
-    parse_es_structure(file_path="data_adapter/tests/test_data/test_structures/processes.csv")
-    pass
+    print(expected_output.parameter)
+    parse_es_structure(
+        file_path=r"test_data\test_structures\SEDOS_Prozesse&Parameter.xlsx",
+        output_path=r"test_data\test_structures\SEDOS_es_structure_test_output.xlsx",
+    )
+
+    func_output = pd.read_excel(r"test_data\test_structures\SEDOS_es_structure_test_output.xlsx")
+
+    assert expected_output == func_output
